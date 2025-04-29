eee915380a99b45a4b27708617ee23bd7d8486b2
Jordan Liu <jordan@liu.com.jm>
2025-04-29 13:15:16 -0500

    chore: update readme

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,12 @@ import { initBrowser, cleanupBrowser } from "lib/utils/browser";
 
 const port = process.env.PORT || 3000;
 
-// Initialize the browser when the server starts
 await initBrowser();
 
 const app = new Elysia().use(screenshot).listen(port);
 
 console.log(`🚀 Server is running at ${app.server?.hostname}:${port}`);
 
-// Handle cleanup on process termination
 process.on("SIGINT", async () => {
   console.log("Shutting down gracefully...");
   await cleanupBrowser();
